07dd691179b61cbd526e29d29329cf346f9ecccb
ESHUshri202 <eshushrivastava1106@gmail.com>
2025-04-28 16:47:28 +0530

    flask_await

--- app.py
+++ app.py
@@ -30,14 +30,14 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 translator = Translator()
 
 @app.route('/translate', methods=['POST'])
-def translate_text():
+async def translate_text():
     data = request.get_json()
     input_text = data.get('text', '')
 
     if not input_text:
         return jsonify({'error': 'No text provided'}), 400
 
-    translated = translator.translate(input_text, dest='en')
+    translated = await translator.translate(input_text, dest='en')
     return jsonify({'translated_text': translated.text})
 
 if __name__ == '__main__':
