5d2736c859f1965bfa1e4044868db088f48e37f1
ESHUshri202 <eshushrivastava1106@gmail.com>
2025-04-28 16:38:38 +0530

    app.py

--- app.py
+++ app.py
@@ -2,22 +2,21 @@ from flask import Flask, jsonify, request
 from googletrans import Translator
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-CORS(app,resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 translator = Translator()
 
-@app.route('/translate',methods=['POST'])
-async def translate_text():
+@app.route('/translate', methods=['POST'])
+def translate_text():
     data = request.get_json()
     input_text = data.get('text', '')
 
     if not input_text:
         return jsonify({'error': 'No text provided'}), 400
 
-    translated = await translator.translate(input_text, dest='en')
+    translated = translator.translate(input_text, dest='en')
     return jsonify({'translated_text': translated.text})
 
 if __name__ == '__main__':
     port = 5000
-    app.run(host="0.0.0.0",port= port,debug=True)        
+    app.run(host="0.0.0.0", port=port, debug=True)
